0d5e2e97c59357c9065a4b57a22b73b919f39095
Helvécio Neto <helvecioneto77@gmail.com>
2020-04-30 14:03:39 -0300

    Adicionando porta do heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
+import 'dotenv/config';
 import app from './app';
 
-app.listen(3333); // apontar para uma porta do computador e conseguir enviar informações atraves dela 3333 padrao
+app.listen(process.env.PORT || 3333); // apontar para uma porta do computador e conseguir enviar informações atraves dela 3333 padrao
 
 // o nodemon serve basicamente para nao precisar ficar ligando/desligando o servidor toda vez que ocorre uma alteracao
